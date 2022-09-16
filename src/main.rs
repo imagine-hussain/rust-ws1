f00b52def7fb207eb8c5001fff3330f4d308d80e
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-16 23:57:58 +1000

    finish diagonals

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 // use bmp;
-use std::io;
 use io::{stdout, Write};
+use std::io;
 
+#[derive(Debug)]
 struct Canvas {
     path: String,
     width: u32,
     height: u32,
-    image: bmp::Image
+    image: bmp::Image,
+}
+
+#[derive(Debug)]
+struct Point {
+    x: u32,
+    y: u32,
 }
 
 fn draw_pixel(path: String) {
@@ -15,21 +22,35 @@ fn draw_pixel(path: String) {
         Ok(image) => image,
         Err(_) => new_canvas(pathc, 100, 100),
     };
-    match image.draw_pixel(50, 50, None) {
+
+    let p1 = Point { x: 0, y: 0 };
+    let p2 = Point { x: 20, y: 20 };
+    let p3 = Point { x: 30, y: 30 };
+
+    match image.draw_pixel(p1, None) {
         Ok(_) => (),
         Err(_) => println!("Error drawing pixel"),
     };
-    match image.draw_pixel(0, 0, None) {
+    match image.draw_pixel(p2, None) {
         Ok(_) => (),
         Err(_) => println!("Error drawing pixel"),
     };
-    image.draw_pixel(0, 50, None).expect("Error drawing pixel");
+    image.draw_pixel(p3, None).expect("Error drawing pixel");
     image.save(None).expect("This should save correctly.");
 }
 
-
-
-
+fn draw_diagonal_image(path: String) {
+    let pathc = path.clone();
+    let mut image = match open_canvas(path) {
+        Ok(image) => image,
+        Err(_) => new_canvas(pathc, 100, 100),
+    };
+    image.draw_line(Point { x: 0, y: 0 }, Point { x: image.width - 1, y: image.height - 1})
+        .expect("Error drawing diagonal line");
+    image.draw_line(Point { x: 0, y: image.height - 1}, Point { x: image.width - 1, y: 0 })
+        .expect("Error drawing diagonal line");
+    image.save(None).expect("This should save correctly.");
+}
 
 fn main() {
     let path = std::env::args().nth(1).expect("You must provide a path.");
@@ -43,9 +64,10 @@ fn main() {
 
     match op.as_str() {
         "pixel\n" => draw_pixel(path),
-        _ =>  {
+        "diagonals\n" => draw_diagonal_image(path),
+        _ => {
             eprintln!("The operation {op} was not recognised!");
-        },
+        }
     }
 
     // let op = stdin.read();
@@ -89,9 +111,14 @@ fn main() {
 }
 
 // Used for creating
-fn new_canvas (path: String, width: u32, height: u32) -> Canvas {
+fn new_canvas(path: String, width: u32, height: u32) -> Canvas {
     let image = bmp::Image::new(width, height);
-    Canvas { path, width, height, image }
+    Canvas {
+        path,
+        width,
+        height,
+        image,
+    }
 }
 
 fn open_canvas(path: String) -> Result<Canvas, bmp::BmpError> {
@@ -99,35 +126,47 @@ fn open_canvas(path: String) -> Result<Canvas, bmp::BmpError> {
         Ok(i) => i,
         Err(e) => return Err(e),
     };
-    Ok(Canvas { path, width: image.get_width(), height: image.get_height(), image })
+    Ok(Canvas {
+        path,
+        width: image.get_width(),
+        height: image.get_height(),
+        image,
+    })
 }
 
 fn image_to_canvas(image: bmp::Image, path: String) -> Canvas {
-    Canvas { path, width: image.get_width(), height: image.get_height(), image }
+    Canvas {
+        path,
+        width: image.get_width(),
+        height: image.get_height(),
+        image,
+    }
 }
 
 #[derive(Debug)]
 enum CanvasError {
-    BmpError(bmp::BmpError),
-    IoError(io::Error),
-    OutBoundsError
+    Bmp(bmp::BmpError),
+    Io(io::Error),
+    InvalidPoint(String),
+    InvalidPoints(String),
+    OutBounds(Point),
 }
 
 impl Canvas {
-    fn draw_pixel(&mut self, x: u32, y: u32, color: Option<bmp::Pixel>) -> Result<(), CanvasError> {
-        if self.out_bounds(x, y) {
-            return Err(CanvasError::OutBoundsError);
+    fn draw_pixel(&mut self, p: Point, color: Option<bmp::Pixel>) -> Result<(), CanvasError> {
+        if self.out_bounds(&p) {
+            return Err(CanvasError::OutBounds(p));
         }
         let pixel = match color {
             Some(c) => c,
-            None => bmp::Pixel::new(255, 255, 255)
+            None => bmp::Pixel::new(255, 255, 255),
         };
-        self.image.set_pixel(x, y, pixel);
+        self.image.set_pixel(p.x, p.y, pixel);
         Ok(())
     }
 
-    fn out_bounds(&self, x: u32, y: u32) -> bool {
-        x > self.width || y > self.height
+    fn out_bounds(&self, p: &Point) -> bool {
+        p.x > self.width || p.y > self.height
     }
 
     fn save_image(&self) -> io::Result<()> {
@@ -144,4 +183,69 @@ impl Canvas {
             Err(e) => Err(e),
         }
     }
+
+    fn draw_line_vertical(&mut self, p1: Point, p2: Point) -> Result<(), CanvasError> {
+        // Draw a line with a constant `x` value
+        if p1.x != p2.x {
+            return Err(CanvasError::InvalidPoint(String::from(
+                "Points must have the same x value",
+            )));
+        }
+        for i in p1.y..p2.y {
+            let p = Point { x: p1.x, y: i };
+            self.draw_pixel(p, None).expect("Error drawing pixel");
+        }
+        Ok(())
+    }
+
+    fn draw_line_horizontal(&mut self, p1: Point, p2: Point) -> Result<(), CanvasError> {
+        // Horizontal line has fixed `y` value
+        if p1.y != p2.y {
+            return Err(CanvasError::InvalidPoint(String::from(
+                "Points must have the same y value",
+            )));
+        }
+        let y = p1.y;
+        for x in p1.x..p2.x {
+            let p = Point { x, y };
+            self.draw_pixel(p, None).expect("Error drawing pixel");
+        }
+        Ok(())
+    }
+
+
+    fn draw_line(&mut self, p1: Point, p2: Point) -> Result<(), crate::CanvasError> {
+        // Increment along `x` values with gradient of y
+        if self.out_bounds(&p1) {
+            return Err(CanvasError::OutBounds(p1));
+        }
+        if self.out_bounds(&p2) {
+            return Err(CanvasError::OutBounds(p2));
+        }
+        if p1.x == p2.x {
+            return self.draw_line_vertical(p1, p2);
+        }
+        if p1.y == p2.y {
+            return self.draw_line_horizontal(p1, p2);
+        }
+
+        let (start, end) = if p1.x < p2.x { (p1, p2) } else { (p2, p1) };
+
+        println!("Drawing line from {:?} to {:?}", start, end);
+        let dy: f64 = end.y as f64 - start.y as f64;
+        println!("done y");
+        let dx: f64 = end.x as f64 - start.x as f64;
+        println!("done x");
+        println!("dy: {}, dx: {}", dy, dx);
+        let gradient = dy / dx;
+
+        let mut j = start.y;
+        for i in start.x..end.x {
+            self.draw_pixel(Point { x: i, y: j }, None)?;
+            j = (gradient * (i - start.x) as f64 + start.y as f64) as u32;
+        }
+
+        Ok(())
+    }
+
 }
